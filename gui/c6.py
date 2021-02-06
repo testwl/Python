85151afbb49499d26de9101a70da49a346af663f
wanglin <wanglin@100.me>
2021-02-06 23:29:22 +0800

    删除

--- gui/c6.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import logging
-def run_shell(command):
-    cmd = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    for info in iter(cmd.stdout.readline, 'utf8'):
-        logging.info(info)
-
-cmd = 'adb logcat'
-run_shell(cmd)
